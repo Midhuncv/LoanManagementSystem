4e8bc9e232ec26767d05adb05cc0789103892706
Midhun <midhuncv728@gmail.com>
2025-03-19 17:03:59 -0400

    view pythonanywhere

--- LoanApp/urls.py
+++ LoanApp/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('RegsiterUserView/',RegsiterUserView.as_view(),name='RegsiterUserView'),
     path('verify-otp/', VerifyOTPView.as_view(), name='verify-otp'),
     path('LoanCreateView/',LoanCreateView.as_view(),name='LoanCreateView'),
-    path('NewRender/',NewRender.as_view(),name='NewRender')
+    path('PythonanywhereRender/',PythonanywhereRender.as_view(),name='PythonanywhereRender')
 ]
